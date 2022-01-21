f9d2040ca653e5169ecbcf079f9982889e66107d
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-01-21 07:52:16 +0100

    models added

--- app/database.py
+++ app/database.py
@@ -7,3 +7,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://<postgres>:<vendremecca123>@localhost/fa
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine )
+
+Base = declarative_base()
