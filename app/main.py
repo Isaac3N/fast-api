50c1d40a26aa1c8b6f20755d2a816728fc7a8f7e
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-01-22 00:48:39 +0100

    updated models for sqlalchemy

--- app/main.py
+++ app/main.py
@@ -7,8 +7,20 @@ from random import  randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor 
 import time 
+from sqlalchemy.orm import Session
+from . import models 
+from .database import SessionLocal, engine 
+
+models.Base.metadata.create_all(bind=engine) #for connecting to the database
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel): #this is an extended base model using pydantic 
