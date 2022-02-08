ba82bb588a79e7c8e3461a05bc2131dd952f20da
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-02-08 17:57:15 +0100

    updates

--- app/main.py
+++ app/main.py
@@ -142,7 +142,7 @@ def delete_post(id:int, db: Session = Depends(get_db)):
     return Response(status_code= status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post:Post, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post:Post, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title= %s, content = %s, published = %s WHERE id = %s 
     #     RETURNING *""", 
     #         (post.title, post.content, post.published, (str(id))))
@@ -151,13 +151,13 @@ def update_post(id: int, post:Post, db: Session = Depends(get_db)):
 
     post_query = db.query(models.Post).filter(models.Post.id == id )
 
-    post = post_query.first()
+    updated_post = post_query.first()
 
-    if post == None:
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
             detail= f"post with id: {id} does not exist" )
 
-    post_query.update(post.dict(), synchronize_session=False)
+    post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return {"data": post_query.first()}
 
